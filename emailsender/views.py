fd54d61b4582edb2859d27e4a56eaa4a556b2c84
shakeeb <shakeebanwar250@gmail.com>
2022-03-10 19:22:28 +0500

    done

--- emailsender/views.py
+++ emailsender/views.py
@@ -797,9 +797,7 @@ class screenshotdetail(APIView):
 class referalCompaignCron(APIView):
     def post(self,request):
         try:
-            # emailList = ["jawadsheikh224@gmail.com","test@evolutionmc.com.au","test1@evolutionmc.com.au","test2@evolutionmc.com.au","test3@evolutionmc.com.au","Shabih_haider1@outlook.com","shakeebanwar250@gmail.com","shoaibbilal101@gmail.com","mf4639@gmail.com"]
-
-            emailList = ["shakeebanwar250@gmail.com"]
+            emailList = ["jawadsheikh224@gmail.com","test@evolutionmc.com.au","test1@evolutionmc.com.au","test2@evolutionmc.com.au","test3@evolutionmc.com.au","Shabih_haider1@outlook.com","shakeebanwar250@gmail.com","shoaibbilal101@gmail.com","mf4639@gmail.com"]
 
             fetchPattern = referalTemplate.objects.all().first()
             if fetchPattern:
